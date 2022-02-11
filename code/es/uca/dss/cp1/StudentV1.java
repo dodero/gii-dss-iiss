17ec82f17335eed5001bcb3c41fafb4afbf9353d
ucam <ucam@MUDIAINFOR108>
2022-02-11 13:10:00 +0100

    Code CP 1 improved

--- code/es/uca/dss/cp1/StudentV1.java
+++ code/es/uca/dss/cp1/StudentV1.java
@@ -6,7 +6,7 @@ public class StudentV1 implements Comparable<StudentV1> {
 	
 	Comparable<Object> id;
 	
-	public StudentV1(Object id) {
+	public StudentV1(Object id) throws ClassCastException {
 		this.id = (Comparable<Object>) id;
 	}
 	
@@ -29,38 +29,42 @@ public class StudentV1 implements Comparable<StudentV1> {
 	
 	
 	public static void main(String[] args) {
+		
 		StudentV1 a = new StudentV1(Integer.valueOf(1));
 		StudentV1 b = new StudentV1(new String("B"));
-		StudentV1 c = new StudentV1(Integer.valueOf(123));
+		StudentV1 c = new StudentV1(Double.valueOf(123.5));
+		StudentV1 d = new StudentV1(Integer.valueOf(300));
 		
-		System.out.println("Name " + a.toString());
-		System.out.println("Name " + c.toString());
+		System.out.println("Name a " + a.toString());
+		System.out.println("Name b " + b.toString());
+		System.out.println("Name c " + c.toString());
 		
 		try {
-		System.out.println("Comp " + a.compareTo(b));
+		System.out.println("Comp a<-->b: " + a.compareTo(b));
 		
 		} catch (ClassCastException e) {
 			e.printStackTrace();
 		}
 		
-		System.out.println("Comp " + a.compareTo(c));
-		
-		
-		
+		try {
+			System.out.println("Comp a<-->d: " + a.compareTo(d));
+			
+		} catch (ClassCastException e) {
+				e.printStackTrace();
+		}
+				
+				
+		// Example of ordering of student with comparable ID
 		StudentV1 [] students = { new StudentV1("cs01"),
 	            new StudentV1("cs21"),
 	            new StudentV1("cs11"),
 	            new StudentV1("cs08") };
 
-	      System.out.println("Before sorting");
+	      System.out.println("\n\nBefore sorting");
 	      System.out.println(Arrays.toString(students));
-
 	      Arrays.sort(students);
-
 	      System.out.println("\n\nAfter sorting by Comparable");
 	      System.out.println(Arrays.toString(students));
-		
-		
 	}
 	
 }
--- code/es/uca/dss/cp1/StudentV2.java
+++ code/es/uca/dss/cp1/StudentV2.java
@@ -23,40 +23,45 @@ public class StudentV2<K extends Comparable<K>>
 		return this.id.compareTo(other.id);				
 	}
 
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		StudentV2<Integer> a = new StudentV2<>(Integer.valueOf(2));
 		StudentV2<String>  b = new StudentV2<>(new String("C"));
-		StudentV2<Integer> c = new StudentV2<>(223);
+		StudentV2<Double> c = new StudentV2<>(223.5);
+		StudentV2<Integer> d = new StudentV2<>(200);
 		
-		System.out.println("Name " + a.toString());
-		System.out.println("Name " + b.toString());
-		System.out.println("Name " + c.toString());
+		System.out.println("Name a " + a.toString());
+		System.out.println("Name b " + b.toString());
+		System.out.println("Name c " + c.toString());
 		
-		//System.out.println("Comp " + a.compareTo(b));
+		// Now the following line produces an error, why?
+		//System.out.println("Comp a<-->b: " + a.compareTo(b));
+				
+		System.out.println("Comp a<-->d: " + a.compareTo(d));
 		
-		
-		System.out.println("Comp " + a.compareTo(c));
-		
-
+		// Example of ordering of student with comparable ID
 		StudentV2[] students = { new StudentV2<String>("cs01"),
 	            new StudentV2<String>("cs21"),
 	            new StudentV2<String>("cs11"),
 	            new StudentV2<String>("cs08") };
 
-	      System.out.println("Before sorting");
-	      System.out.println(Arrays.toString(students));
-
-	      // Note this is for the array
-	      Arrays.sort(students);
-
-	      System.out.println("\n\nAfter sorting by Comparable");
-	      System.out.println(Arrays.toString(students));
+	     System.out.println("\n\nBefore sorting");
+	     System.out.println(Arrays.toString(students));
+	     Arrays.sort(students);
+	     System.out.println("\n\nAfter sorting by Comparable");
+	     System.out.println(Arrays.toString(students));
+	     System.out.println("\n\n");
+	     
+	     // Now examples of using complex identifier
+	     StudentV2<ComplexIdentifier> sci1 = 
+					new StudentV2<>(new ComplexIdentifier(3, "tres"));
+			
+		 StudentV2<ComplexIdentifier> sci2 = 
+					new StudentV2<>(new ComplexIdentifier(3, "cuatro"));
 		
-		
-	
-
+		 System.out.println("Name sci1 " + sci1.toString());
+		 System.out.println("Name sci2 " + sci2.toString());
+		 System.out.println("Comp sci1<-->sci2:" + sci1.compareTo(sci2));					
 	}
 
 }
